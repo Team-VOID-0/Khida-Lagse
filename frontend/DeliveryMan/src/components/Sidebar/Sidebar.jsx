48219af3e5957003293ef0fa958429ada4f1a4fc
Akibul Hasan Anik <ahanik099@gmail.com>
2024-12-24 23:04:22 +0600

    update sidebar

--- frontend/DeliveryMan/src/components/Sidebar/Sidebar.jsx
+++ frontend/DeliveryMan/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Sidebar.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { IoLogOut } from "react-icons/io5";
 import { MdSettingsSuggest } from "react-icons/md";
 import { BiTask } from "react-icons/bi";
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
 const Sidebar = () => {
+  const navigate = useNavigate();  // Initialize the useNavigate hook
+
+  const handleLogout = () => {
+    // Remove the token from localStorage
+    localStorage.removeItem('access_token');
+    // Redirect to the home page ('/')
+    navigate('/');
+  };
+
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
@@ -14,21 +23,21 @@ const Sidebar = () => {
           <LocalShippingIcon className='icons'/>
           <p>Delivery</p>
         </NavLink>
-        <NavLink to='/completed' className="sidebar-option">
+        {/* <NavLink to='/completed' className="sidebar-option">
           <BiTask className='icons'/>
           <p>Completed</p>
-        </NavLink>
+        </NavLink> */}
         <NavLink to='/profile' className="sidebar-option">
           <MdSettingsSuggest className='icons'/>
           <p>Settings</p>
         </NavLink>
-        <NavLink to='/' className="sidebar-option">
+        <div className="sidebar-option" onClick={handleLogout}>
           <IoLogOut className='icons'/>
           <p>Logout</p>
-        </NavLink>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
